ccd48b7ef906075a7106629c73b54b80b1fa9792
Rob Tucker <rtucker@mozilla.com>
2012-12-19 05:12:30 -0800

    Added package dependency validation

--- site_media/javascripts/jquery.validate.js
+++ site_media/javascripts/jquery.validate.js
@@ -349,14 +349,26 @@ $.extend($.validator, {
 			return this.valid();
 		},
 
-		checkForm: function() {
+		/*checkForm: function() {
 			this.prepareForm();
 			for ( var i = 0, elements = (this.currentElements = this.elements()); elements[i]; i++ ) {
 				this.check( elements[i] );
 			}
 			return this.valid();
-		},
-
+		},*/
+        checkForm: function() {
+            this.prepareForm();
+            for ( var i = 0, elements = (this.currentElements = this.elements()); elements[i]; i++ ) {
+                if (this.findByName( elements[i].name ).length != undefined && this.findByName( elements[i].name ).length > 1) {
+                    for (var cnt = 0; cnt < this.findByName( elements[i].name ).length; cnt++) {
+                        this.check( this.findByName( elements[i].name )[cnt] );
+                    }
+                } else {
+                    this.check( elements[i] );
+                }
+            }
+        return this.valid();
+        },
 		// http://docs.jquery.com/Plugins/Validation/Validator/element
 		element: function( element ) {
 			element = this.validationTargetFor( this.clean( element ) );
